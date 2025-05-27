c9d58f66f84a8d82f81353e17d040b205f6a4f37
Pavel Kalinin <meskaline213+git@yandex.ru>
2025-05-27 11:22:39 +0300

    Add SQL script and CI workflow

---
+++ query.sql
@@ -0,0 +1,10 @@
+-- Получить первую строку таблицы
+SELECT * FROM fr.fram_acc LIMIT 1;
+
+-- Все РНК крыс
+SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
+FROM full_region fr, rfamseq rf, taxonomy tx
+WHERE rf.ncbi_id = tx.ncbi_id
+  AND fr.rfamseq_acc = rf.rfamseq_acc
+  AND tx.ncbi_id = 10116
+  AND is_significant = 1;
